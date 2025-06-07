96facb98eeab42eeefe1721fd173256245aecd55
BrenyQT <brendan.quinn2003@gmail.com>
2025-06-07 17:29:53 +0100

    final touches

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",  // <-- make sure your source files are included here
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
